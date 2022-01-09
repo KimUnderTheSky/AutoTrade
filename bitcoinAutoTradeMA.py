980abc101b09142f80e136770113902c24d9f746
KimUnderTheSky <cjsgksla@ajou.ac.kr>
2022-01-09 12:54:09 +0900

    한번만 매수조건 수정

--- bitcoinAutoTradeMA.py
+++ bitcoinAutoTradeMA.py
@@ -4,7 +4,7 @@ import datetime
 
 access = "your-access"
 secret = "your-secret"
-flag = True
+flag = True #1번만 매수
 
 df_public = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
 ago_range = df_public.iloc[0]['high'] - df_public.iloc[0]['low'] #전일변동성
@@ -60,11 +60,11 @@ while True:
             if target_price < current_price and ma5 < current_price: #현재가가 target price이상이고 5일 이평선 이상일때
                 krw = get_balance("KRW")
                 while True:
-                    if(flag):
+                    if(flag): #flag가 True이면 매수
                         if krw > 5000: #krw가 내 잔고 내 전재산
                             upbit.buy_market_order("KRW-XRP", round(0.2/round(ago_range/current_price,2),2)*(krw*0.9995)) #구매
                             # 변동성 조절로 내재산2% 코인자산 20% 변동 픽스?
-                            flag = False
+                            flag = False #한번 사면 False값 넣음
 
         else:
             XRP = get_balance("XRP")
