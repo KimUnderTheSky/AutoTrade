4cec3808bccf728f22f78725359005fbc77d50ea
KimUnderTheSky <cjsgksla@ajou.ac.kr>
2022-01-09 12:57:19 +0900

    저장되라고

--- bitcoinAutoTradeMA.py
+++ bitcoinAutoTradeMA.py
@@ -4,7 +4,7 @@ import datetime
 
 access = "your-access"
 secret = "your-secret"
-flag = True #1번만 매수
+flag = True #1번만 매수 저장되라고
 
 df_public = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
 ago_range = df_public.iloc[0]['high'] - df_public.iloc[0]['low'] #전일변동성
