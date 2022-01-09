6f071020e9636d724c3875ded3cca62c5d5614de
KimUnderTheSky <cjsgksla@ajou.ac.kr>
2022-01-09 15:29:49 +0900

    이제 이게 완성본

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -4,13 +4,15 @@ import datetime
 
 access = "your-access"
 secret = "your-secret"
+flag = True #1번만 매수 저장되라고 저장안되냐고
 
+df_public = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
+ago_range = df_public.iloc[0]['high'] - df_public.iloc[0]['low'] #전일변동성
 
-def get_target_price(ticker, k): #이부분을 바꿔야 새로운 전략이 됨
+def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k 
-    #코인은 다음날 시가와 붙어있어서 iloc이 다음 날 시가와 같다.
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
@@ -19,7 +21,13 @@ def get_start_time(ticker):
     start_time = df.index[0]
     return start_time
 
-def get_balance(ticker): #ticker 어떤 코인인지
+def get_ma5(ticker):
+    """5일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=5) #file가져오기
+    ma5 = df['close'].rolling(5).mean().iloc[-1] #5일씩 나눠서 평균내라 = 이평선구하는 것.
+    return ma5
+
+def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
@@ -42,21 +50,28 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC") #시작시간 get_start_time함수로 OHLCV조회할때 일봉으로 조회하면 그날의 시작시간이 나옴 9시로
-        end_time = start_time + datetime.timedelta(days=1) #끝나는시간 #1일을 더해준값
+        start_time = get_start_time("KRW-XRP")
+        end_time = start_time + datetime.timedelta(days=1)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10): #10초를 빼줘서 8시 59분 50초까지 돌아가게해줌
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price: #목표가보다 현재가가 높다면
-                krw = get_balance("KRW") # 내잔고 확인
-                if krw > 5000: #잔고에 5000원 이상일때 
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995) #비트코인 매수, 수수료 0.05고려
-        else: # 8시 59분 50초부터 9시까지 사이에는
-            btc = get_balance("BTC") #전량매도
-            if btc > 0.00008: #5000원 이상이면? (BTC가격으로 환산한듯)
-                upbit.sell_market_order("KRW-BTC", btc*0.9995) # 수수료 고려해서 
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-XRP", 0.5)
+            ma5 = get_ma5("KRW-XRP")
+            current_price = get_current_price("KRW-XRP")
+            if target_price < current_price and ma5 < current_price: #현재가가 target price이상이고 5일 이평선 이상일때
+                krw = get_balance("KRW")
+                while True:
+                    if(flag): #flag가 True이면 매수
+                        if krw > 5000: #krw가 내 잔고 내 전재산
+                            upbit.buy_market_order("KRW-XRP", round(0.2/round(ago_range/current_price,2),2)*(krw*0.9995)) #구매
+                            # 변동성 조절로 내재산2% 코인자산 20% 변동 픽스?
+                            flag = False #한번 사면 False값 넣음
+
+        else:
+            XRP = get_balance("XRP")
+            if XRP > round(5000/get_current_price("KRW-XRP"),3):
+                upbit.sell_market_order("KRW-XRP", XRP*0.9995)
+                flag = True
         time.sleep(1)
-    except Exception as e:#오류 발생시 잡아주는 코드
+    except Exception as e:
         print(e)
         time.sleep(1)
