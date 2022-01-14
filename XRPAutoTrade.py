e234c1e71469232b254a6d847c0a9d2256adeb09
KimUnderTheSky <cjsgksla@ajou.ac.kr>
2022-01-14 17:09:12 +0900

    코드수정

--- XRPAutoTrade.py
+++ XRPAutoTrade.py
@@ -64,7 +64,7 @@ while True:
                             df_public = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
                             ago_range = df_public.iloc[0]['high'] - df_public.iloc[0]['low'] #전일변동성
                             ago_range1 = ago_range/current_price #(0.2/ago_range1)/2이 퍼센테이지로 나와야함
-                            total = ((0.2/ago_range1)/(100*4))*(krw*0.9995)  # 변동성 조절로 내재산2% 코인자산 20% 변동 픽스
+                            total = ((0.05/ago_range1)/5)*(krw*0.9995)  # 변동성 조절로 내재산2% 코인자산 20% 변동 픽스
                              #구매
                             if total > 5000: # 총투자 금액이 5000이상이어야 buy
                                 upbit.buy_market_order("KRW-XRP", total)
